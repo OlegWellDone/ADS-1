244cf9d15d21353de04b30605a32f01121b163ff
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-02-21 22:42:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,7 +19,7 @@ uint64_t chicha = 2;
 uint64_t id = 2;
 while(id != n) {
 chicha++;
-if(checkPrime(chicha)) {
+if (checkPrime(chicha)) {
 id++;
 }
 }
@@ -28,11 +28,12 @@ return chicha;
 
 uint64_t nextPrime(uint64_t value) {
 uint64_t blizko;
-while(uint64_t i < value) {
-if(checkPrime(value + i)) {
+uint64_t i;
+while(i < value) {
+if (checkPrime(value + i)) {
   blizko = value + i;
 }
-  if(checkPrime(value - i)) {
+  if (checkPrime(value - i)) {
   blizko = value - i;
 }
 }
@@ -43,7 +44,7 @@ uint64_t sumPrime(uint64_t hbound) {
 uint64_t i = 2;
 uint64_t sum = 0;
 while(i < hbound) {
-if(checkPrime(i)) {sum = sum+i;}
+if (checkPrime(i)) {sum = sum+i;}
 i++;
 }
 return sum;
